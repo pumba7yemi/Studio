86193ac828daed068d3b99b95f55c9ed191495cf
pumba7yemi <studio@corae.app>
2025-07-31 17:16:08 +0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <div className="p-4 text-xl font-bold">Welcome to corAe Studio</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
